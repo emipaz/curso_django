02376d0d6be94f5e4fd261584237ba4cbd0887da
Emiliano Passarello <emiliano.passarello@graduadosfiuba.com>
2022-03-11 00:56:56 +0000

    jugamos con css

---
+++ site/css/blocks.cs
@@ -0,0 +1,49 @@
+
+/* This is a partial blocks.css, you need to add more rules
+   to this file to complete the assignment.  Do not add any CSS
+   styling to the original index.htm / start.htm */
+
+
+/* Here is one of many  CSS rules you will need... */
+
+
+body {
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+div{
+    width: 25%;
+    position: fixed;
+    border: solid 5px;
+    padding: 5px;
+    margin: 5px;}
+
+
+#one {border-color: "blue";
+    bottom: 1%;
+    right: 1%;
+}
+#two {border-color: "green";
+    top: 1%;
+    right: 1%;}
+
+#tree {border-color:"orange";
+        top: 1%; left: 1% }
+
+#four {border-color:yellow;
+        bottom: 1%; left: 1% }
+
+
+
+
+
+#link {text-aling: center}
+#link a{
+    color: #fefefe;
+    text-decoration: none;
+    background-color: #354eb1;
+    font-size: 14px;
+    font-weight: 500;
+    padding: 1px;}
+
+
